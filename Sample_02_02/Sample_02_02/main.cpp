c659f6e374832009328366c2b682a32d6f00721f
Jun Shimura <junshimura@gmail.com>
2025-09-29 17:31:24 +0900

    チェック柄

--- Sample_02_02/Sample_02_02/main.cpp
+++ Sample_02_02/Sample_02_02/main.cpp
@@ -41,16 +41,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 頂点配列を定義
     SimpleVertex vertices[] = {
         {
-            {-0.5f, -0.5f, 0.0f},
-            { 1.0f, 0.0f, 0.0f }
+			{-0.5f, -0.5f, 0.0f},   // 左下
+			{ 1.0f, 0.0f, 0.0f }    // 赤
         },
         {
-            { 0.0f, 0.5f, 0.0f },
-            { 0.0f, 1.0f, 0.0f }
+			{-0.5f, 0.5f, 0.0f},    // 左上
+            { 1.0f, 0.0f, 0.0f }    // 赤
         },
         {
-            { 0.5f, -0.5f, 0.0f },
-            { 0.0f, 0.0f, 1.0f }
+			{ 0.5f, 0.5f, 0.0f },   // 右上  
+			{ 0.0f, 1.0f, 0.0f }    // 緑
+        },
+        {
+			{ 0.5f, -0.5f, 0.0f },  // 右下
+			{ 0.0f, 0.0f, 1.0f }    // 青
         }
     };
 
@@ -61,7 +65,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 5. 三角形のインデックスバッファを作成
     //インデックス配列
     uint16_t indices[] = {
-        0,1,2
+		0,1,2,   // 三角形1 ◤
+		0,2,3    // 三角形2 ◢
     };
     IndexBuffer triangleIB;
     triangleIB.Init(sizeof(indices), 2);
@@ -93,7 +98,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         // 5. インデックスバッファを設定
         renderContext.SetIndexBuffer(triangleIB);
         // 6. ドローコール
-        renderContext.DrawIndexed(3);
+        renderContext.DrawIndexed(6);
+        // renderContext.DrawIndexed(sizeof(indices)/sizeof(indices[0]);
 
         /// //////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
